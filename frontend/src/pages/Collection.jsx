13112d78acb3f60c2b2286c46a78729c95725629
Shreya Navnath Chavan <shreyanchavan135@gmail.com>
2026-02-01 15:15:33 +0530

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
-import { assets } from '../assets';
 import Title from '../components/Title';
 import ProductItems from '../components/ProductItems';
 
