cdce3a4c3e49e93ae205319d267a9647a3809054
Shreya Navnath Chavan <shreyanchavan135@gmail.com>
2026-02-01 15:32:39 +0530

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -159,7 +159,7 @@ const Collection = () => {
               name={item.name}
               id={item._id}
               price={item.price}
-              image={item.image[0]}
+              image={item.image[index]}
             />
           ))}
         </div>
